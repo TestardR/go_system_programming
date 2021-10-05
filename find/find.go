7d7c4548e7244bf60560b356dd485b93ce5243b3
TestardR <romain.rtestard@gmail.com>
2021-10-05 19:37:41 +0200

    feat(cmd): add new cmds

---
+++ find/find.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	flag.Parse()
+	flags := flag.Args()
+	if len(flags) == 0 {
+		fmt.Println("Not enough arguments!")
+		os.Exit(1)
+	}
+	Path := flags[0]
+	err := filepath.Walk(Path, walkFunction)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func walkFunction(path string, info os.FileInfo, err error) error {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	mode := fileInfo.Mode()
+	if mode.IsDir() || mode.IsRegular() {
+		fmt.Println(path)
+	}
+
+	return nil
+}
